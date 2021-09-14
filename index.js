dade11dbf7440d8a5a679a77c30026b7980b0d12
jasonnonstop10 <jay44411@gmail.com>
2021-09-14 18:26:21 +0700

    [setup] final main

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+require("dotenv").config();
 const express = require("express");
+const connectToDatabase = require("./db.js");
 const app = express();
 const port = process.env.PORT || 3000;
-
+const connectMongo = async (req, res, next) => {
+  try {
+    console.log("gg");
+    await connectToDatabase();
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(connectMongo);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
