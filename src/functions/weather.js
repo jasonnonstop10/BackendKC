0b2758982ef7671e109a85c250e1eafee2cfb6bc
jasonnonstop10 <jay44411@gmail.com>
2021-09-23 14:26:11 +0700

    [edit]:art:  weather done

--- src/functions/weather.js
+++ src/functions/weather.js
@@ -5,10 +5,12 @@ const valid_id = mongoose.Types.ObjectId.isValid;
 module.exports.getWeather = async (input) => {
   if (valid_id(input)) {
     const user = await userAuth.findOne({ _id: input, isDeleted: false });
-    let region = user.region;
-    return axios.get(
+    let region = "";
+    region = user.region;
+    const regionJS = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?q=${region}&appid=f6e0fd6155de2dd25d21739c7f998724`
     );
+    return regionJS.data;
   } else {
     throw {
       message: "userid is not defined",
